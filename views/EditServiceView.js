9d1fb09043cec06d85e541b75c4b88b38d66dd39
ThaisPio <89753258+ThaisPio@users.noreply.github.com>
2024-10-14 12:27:40 -0300

    Update EditServiceView.js
    Atualizado

--- views/EditServiceView.js
+++ views/EditServiceView.js
@@ -5,20 +5,22 @@ import { getServiceById, handleUpdateService } from '../controllers/ServiceContr
 const EditServiceView = ({ route, navigation }) => {
     const { serviceId } = route.params;
     const [service, setService] = useState(null);
-
+    
+//Quando o componente é montado ou o serviceId é alterado, a função
     useEffect(() => {
         getServiceById(serviceId, (result) => {
             setService(result);
         });
     }, [serviceId]);
-
+    
+//Verifica se todos os campos necessários (nome do serviço, nome da empresa, descrição) foram preenchidos.
 const updateService = () => {
   // Certifique-se de que todos os valores estão definidos corretamente
   if (!service || !service.service_name || !service.company_name || !service.description) {
     Alert.alert('Erro', 'Por favor, preencha todos os campos corretamente.');
     return;
   }
-  
+  //O preço é convertido para número (numericPrice). Se o valor não for um número válido, exibe um alerta de erro.
   const numericPrice = parseFloat(service.numeric_price); // Converte para número
   
   if (isNaN(numericPrice)) {
@@ -26,13 +28,13 @@ const updateService = () => {
     return;
   }
 
-  // Certifique-se de que está passando os valores corretamente na atualização
+ //Chama handleUpdateService: Se todos os campos forem válidos, a função handleUpdateService é chamada para atualizar o serviço no banco de dados
   handleUpdateService(service.id, service.company_name, service.location, service.service_name, service.category, service.description, numericPrice, () => {
     Alert.alert('Sucesso', 'Serviço atualizado com sucesso!');
     navigation.goBack();
   });
 };
-
+//renderização da interface
     return service ? (
         <View style={styles.container}>
             <TextInput
@@ -70,7 +72,7 @@ const updateService = () => {
         <View><Text>Carregando serviço...</Text></View> // Adiciona o componente Text
     );
 };
-
+//Estilização
 const styles = StyleSheet.create({
     container: {
         flex: 1,
